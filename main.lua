a808efa2a31db5e4eafa31175c59ccb951bcc0f4
Cactus <kokotobecny183@gmail.com>
2025-11-08 22:47:02 +0100

    Rename 'Eternal Hamburg' to 'Emergency Hamburg'

--- main.lua
+++ main.lua
@@ -2,11 +2,15 @@
 local gameId = game.GameId
 
 if gameId == 2992873140 then
-    -- Eternal Hamburg (EH)
+    -- Emergency Hamburg
     loadstring(game:HttpGet("https://raw.githubusercontent.com/CactusDaExploiter/Pribinacek-Hub/refs/heads/main/EH.lua"))()
 elseif gameId == 6331902150 then
     -- Forsaken
     loadstring(game:HttpGet("https://raw.githubusercontent.com/CactusDaExploiter/Pribinacek-Hub/refs/heads/main/Forsaken.lua"))()
+    
+elseif gameId == 111958650 then
+    --Arsenal
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/CactusDaExploiter/Pribinacek-Hub/refs/heads/main/Arsenal.lua"))()
 else
     warn("[Pribinacek Hub] Unsupported Game ID: " .. tostring(gameId))
 end
